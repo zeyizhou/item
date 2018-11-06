1ab361b3d573ec3211a86b3f80076f41e3890f2f
zhou <zzy28101994@gmail.com>
2018-11-06 23:51:40 +0100

    image with auto delete

--- price/models.py
+++ price/models.py
@@ -4,9 +4,10 @@ from django.db import models
 from django.db import models
 import datetime
 from django.utils import timezone
-from django import forms
-import time
 
+from django.dispatch import receiver
+import os
+import time
 
 class  Item(models.Model):
     mark = models.CharField('Mark', max_length=200, blank=True)
@@ -24,3 +25,34 @@ class  Item(models.Model):
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+@receiver(models.signals.post_delete, sender=Item)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `MediaFile` object is deleted.
+    """
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
+
+@receiver (models.signals.pre_save, sender=Item)
+def auto_delete_file_on_change(sender, instance, *args, **kwargs):
+    if not instance.pk:
+        return False
+
+    try:
+        old_image = None
+        if(Item.objects.get (pk=instance.pk).image):
+            old_image = Item.objects.get (pk=instance.pk).image
+        else:
+            return False
+        print(old_image.path)
+    except Item.DoesNotExist:
+        return False
+
+    new_image = instance.image
+    if not old_image == new_image:
+        if os.path.isfile (old_image.path):
+            os.remove (old_image.path)
